8b6c77be7973510629c5a26b19e6a5df78ea75aa
Thiago Fredes <trodrigues02@gmail.com>
2025-09-07 10:45:37 -0400

    Created auth routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,6 +7,8 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
+const authRoutes = require("./routes/authRoutes");
+
 
 const app = express();
 app.use("*",cors());
@@ -28,6 +30,7 @@ const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
 
+app.use('/api/auth', authRoutes);
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
