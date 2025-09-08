9b9f9b0c5dff9810f46c113b54670754ae796921
thiago fredes <trodrigues02@gmail.com>
2025-09-08 14:05:12 -0400

    Created route for login

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -39,7 +39,7 @@ router.post('/register', async (req, res) => {
             password: hash,
             createdAt: new Date(),
         });
-        
+
         const payload = {
             user: {
                 id: newUser.insertedId,
@@ -47,11 +47,67 @@ router.post('/register', async (req, res) => {
         };
 
         const authtoken = jwt.sign(payload, JWT_SECRET);
-        
+
         logger.info('User registered successfully');
-        res.json({authtoken,email});
+        res.json({ authtoken, email });
     } catch (e) {
-         return res.status(500).send('Internal server error');
+        return res.status(500).send('Internal server error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        // Task 4: Check if the password matches the encrypyted password and send appropriate message on mismatch
+        if (theUser) { //only does anything if there is an user in the db
+
+            // the comparison is safely done internally using bcrypt
+            // compares a password previously encrypted with bcrypt to a plain text
+            let result = await bcryptjs.compare(req.body.password, theUser.password);
+
+            // if passwords do not match, abort
+            if (!result) {
+                logger.error("Passwords do not match");
+
+                // this is not really advised, since tells a malicious user
+                // that at least that username exists
+                return res.status(404).json({ error: "Wrong password" });
+            }
+
+            // Task 5: Fetch user details from database
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+
+            // signs the JWT using the secret defined in the .env file
+            jwt.sign(user._id, JWT_SECRET);
+
+            res.json({ authtoken, userName, userEmail });
+        }
+        else {
+            // Task 7: Send appropriate message if user not found
+            logger.error("User not found");
+
+            // this is not really good, as it tells a malicious user
+            // that there is no user with that username in the db
+            return res.status(404).json({ error: "User not found" });
+        }
+    } catch (e) {
+        return res.status(500).send('Internal server error');
     }
 });
 
