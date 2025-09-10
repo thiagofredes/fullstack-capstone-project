107254071f44fabd8d90c6f34a0fd686596aafbf
thiagofredes <trodrigues02@gmail.com>
2025-09-10 13:54:04 -0400

    Fixed bug in product details page

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -14,7 +14,7 @@ function DetailsPage() {
         const authenticationToken = sessionStorage.getItem('auth-token');
         if (!authenticationToken) {
             // Task 1: Check for authentication and redirect
-            navigate('app/login');
+            navigate('/app/login');
         }
 
         // get the gift to be rendered on the details page
@@ -39,7 +39,7 @@ function DetailsPage() {
         // Task 3: Scroll to top on component mount
         window.scrollTo(0, 0)
 
-    }, [productId]);
+    }, [productId, navigate]);
 
 
     const handleBackClick = () => {
